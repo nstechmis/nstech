ad1014256e405ecbb7de5e0239bc78731c2b44d1
Siong-Ui Te <siongui@gmail.com>
2014-12-27 01:06:44 +0800

    config files

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'nstech'
+SITENAME = u'穠纖科技 Nung Sian Tech.'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'theme'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
